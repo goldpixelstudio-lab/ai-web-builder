520f541ec495c3aeb05999fa19216f98372d6a86
Dominik <goldpixelstudio@gmail.com>
2026-03-24 20:00:20 +0100

    Fix: Re-inicjalizacja PostCSS i Tailwind, powrot do fabrycznych ustawien z trybem ciemnym

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: 'class', 
+  darkMode: "class",
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // <--- TO PRZYWRÓCI WSZYSTKIE STYLE
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
